1614691920a9469415e6feb958b44cec1bd3f699
Lucas Gabriel Souza <162371209+luketta07@users.noreply.github.com>
2024-10-29 17:24:52 -0300

    Adicionado código calculo de ganho por mês em python
    Mais um código feito, com os ensinamentos do curso de ADS

---
+++ Calculo-de-ganho-por-mes.py
@@ -0,0 +1,8 @@
+numero=int(input("quanto voce ganha por hora, digite o valor:"))
+
+horas_trabalhadas=int(input("quantas horas voce tem trabalhadas por mes:"))
+
+ meses=3 
+ resultado1=(numero*horas_trabalhadas)
+ resultado2=(resultado1*meses) 
+ print("seu ganho por mes e:", resultado1, " por curiosidade, em 3 meses voce teria " ,resultado2, "")
